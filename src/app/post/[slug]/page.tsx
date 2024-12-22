047eecbc713e48750ba56398070b011c9e76a7e5
azmataliakbar <azmat36@yahoo.com>
2024-12-22 20:42:47 +0500

    fixing bg-colors

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -62,7 +62,7 @@ export default async function PostPage({ params }: { params: { slug: string } })
   // If post is not found, return a 404 page
   if (!post) {
     return (
-      <div className="flex flex-col items-center justify-center h-screen bg-gray-900">
+      <div className="flex flex-col items-center justify-center h-screen bg-gray-500">
         <h1 className="text-red-500 text-4xl font-bold mb-4">Post Not Found</h1>
         <Link href="/" className="text-blue-400 underline">
           &lt;&lt; Back to Home Page
@@ -73,11 +73,11 @@ export default async function PostPage({ params }: { params: { slug: string } })
 
   return (
     <main className="container mx-auto p-4">
-      <article className="bg-gray-500 p-6 rounded-lg shadow-md">
-        <h1 className="text-green-400 text-3xl font-bold hover:underline hover:text-orange-400 hover:scale-y-125 mb-4">
+      <article className="bg-gray-400 p-6 rounded-lg shadow-md">
+        <h1 className="text-black text-3xl font-bold hover:underline hover:text-white hover:scale-y-125 mb-4">
           {post.title}
         </h1>
-        <p className="text-white text-xl md:text-lg lg:text-lg font-bold hover:underline hover:text-yellow-300 hover:scale-y-125 mb-4">
+        <p className="text-black text-lg md:text-lg lg:text-lg font-bold hover:underline hover:text-white hover:scale-y-125 mb-4">
           Published on: {new Date(post.publishedAt).toDateString()}
         </p>
         {/* Display logo image if available */}
@@ -101,8 +101,8 @@ export default async function PostPage({ params }: { params: { slug: string } })
       </article>
 
       {/* About the author section */}
-      <section className="mt-8 p-4 bg-gray-500 rounded-lg">
-        <h3 className="text-white text-lg font-bold hover:underline hover:text-yellow-300 hover:scale-y-125 mb-4">
+      <section className="mt-8 p-4 bg-gray-400 rounded-lg">
+        <h3 className="text-black text-lg font-bold hover:underline hover:text-white hover:scale-y-125 mb-4">
           About the Author
         </h3>
         <div className="flex items-center space-x-4">
@@ -122,7 +122,7 @@ export default async function PostPage({ params }: { params: { slug: string } })
           )}
 
           {/* Author's name */}
-          <p className="text-white font-bold hover:text-yellow-300">
+          <p className="text-black font-bold hover:text-white">
             {post.author?.name || 'No author name available'}
           </p>
         </div>
